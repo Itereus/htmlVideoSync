68b90709b709c03d220733c51b8272eb3e714c13
Itereus <kevinthommesen@gmail.com>
2018-10-12 15:30:57 +0200

    Cleaned up some code. Added comments. Added onload muted playback

--- res/js/script.js
+++ res/js/script.js
@@ -1,21 +1,35 @@
-var vid = document.getElementById("film");
-
-// To be implemented
+// To be implemented properly
 var timeOffset = 0;
-
 // Time here is the duration of the film, in milliseconds
-var videoTotalTime = 36.047;
-var btnPlay = document.getElementById("play");
+var videoTotalTime = 967.5093;
+var vid = document.getElementById("film");
 var time = document.getElementById("vidTime");
 var sysTime = document.getElementById("sysTime");
 var testTime = document.getElementById("testTime");
 var timerId;
+var updateTime;
 var d = new Date();
 var n;
 
+vid.onclick = function() {playDelayed(vid)};
+window.onload = function() {playDelayed(vid)};
 
 function playDelayed(filmKilde) {
+    filmKilde.currentTime = getOffSetTime();
+    filmKilde.play();
 
+    // Syncronize every 5 seconds
+    clearInterval(updateTime);
+    updateTime = setInterval(function() {
+    filmKilde.currentTime = getOffSetTime();
+    }, 5000)
+
+    // Used for debugging
+    // Comment out when done
+    updateTimeText(filmKilde);
+}
+
+function getOffSetTime() {
     // BELOW: Use milliseconds only?
     // Get the Unix timestamp in milliseconds, convert to seconds. Divide by
     // length of video, in seconds with decimal.
@@ -24,15 +38,10 @@ function playDelayed(filmKilde) {
     n = n - Math.floor(n);
     // Set the starttime of the video to the time based on system time.
     timeOffset = n * videoTotalTime;
-    filmKilde.currentTime = timeOffset;
-    filmKilde.play();
-
-    // Both lines below should be removed in final
-    updateTimeText(filmKilde);
-    testPlayDelayed(filmKilde);
+    return timeOffset;
 }
 
-// Code below is for debugging purposes
+// Function is for debugging
 function updateTimeText(filmKilde) {
     clearInterval(timerId);
     timerId = setInterval(function() {
@@ -46,13 +55,3 @@ function updateTimeText(filmKilde) {
     testTime.innerHTML = n;
     }, 1000)
 }
-
-function testPlayDelayed(filmKilde) {
-    d = new Date();
-    n = (d.getTime() / 1000) / videoTotalTime;
-    n = n - Math.floor(n);
-    n = n * timeOffset;
-    testTime.innerHTML = n;
-}
-
-btnPlay.onclick = function() {playDelayed(vid)};
